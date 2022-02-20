961f38a24e3ef54515ce2c3f34317435fea7ce55
Manirakiza Blaise <manirakizablaise@gmail.com>
2022-02-20 16:45:08 +0200

    phone_preloader and hidden validation when I send email

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ links.forEach((links)=>{
 		menu_open.style.display="";
 	});
 });
+
+// swap pour mobile
+
 let touchStart,touchEnd;
 nav.addEventListener("touchstart",(e)=>{
 	touchStart=e.targetTouches[0].clientX;
@@ -43,85 +46,75 @@ nav.addEventListener("touchend",(e)=>{
 // contact form
 
 function succes(name){
-			swal({
-			  title: "hey,"+name.value+"",
-			  text: "Message recu avec success",
-			  icon: "success",
-			  button: "Ok",
-			});
-		}
-		function denied(){
-			swal({
-			  title: "Oups...",
-			  text: "Erreur de Services",
-			  icon: "error",
-			  button: "Ok",
-			  color:"red"
-			});
-		}
-		function input_require(){
-			swal({
-			  title: "Oups...",
-			  text: "Renseigne bien tous les champs svp!!",
-			  icon: "error",
-			  confirmButtonColor:"red",
-			});
-		}
-		function sendMail(nom,email,message)
-		{
-			emailjs.send("service_jznakrk","template_updypd6",{
-				from_name: email,
-				to_name: nom,
-				message: message,
-				});
-
-		}
+	swal({
+	  title: "hey,"+name.value+"",
+	  text: "Message recu avec success",
+	  icon: "success",
+	  button: "Ok",
+	});
+}
+function input_require(){
+	swal({
+	  title: "Oups...",
+	  text: "Renseigne bien tous les champs svp!!",
+	  icon: "error",
+	  confirmButtonColor:"red",
+	});
+}
+function sendMail(nom,email,message)
+{
+	emailjs.send("service_jznakrk","template_updypd6",{
+		from_name: email,
+		to_name: nom,
+		message: message,
+		});
+}
 		// Start validation e-mail
 
 const icon_valid=document.querySelector(".valid");
 const icon_invalid=document.querySelector(".invalid");
 const regex_email=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-		function Valide(){
+function Valide(){
 
-			const nom_prenom=document.querySelector(".name")
-			const mail=document.querySelector(".mail")
-			const message=document.querySelector(".message")
-			const bouton=document.querySelector(".btn_envoye");
+	const nom_prenom=document.querySelector(".name")
+	const mail=document.querySelector(".mail")
+	const message=document.querySelector(".message")
+	const bouton=document.querySelector(".btn_envoye");
 
-			let rep;
+	let rep;
 
-			mail.addEventListener("input",e=>{
-					if(mail.value.match(regex_email)){
-							icon_valid.style.display="flex";
-							icon_invalid.style.display="none";
-							rep=true;
-						}
-					else{
-							icon_invalid.style.display="flex";
-							icon_valid.style.display="none";
-							rep=false;
-						}
-					if(mail.value==""){
-						icon_valid.style.display="none";
-						icon_invalid.style.display="none";
-					}
-				});
-			bouton.addEventListener("click",(e)=>{
-				e.preventDefault()
-
-				if(nom_prenom.value == "" || mail.value=="" || message.value=="" || rep==false)
-				{
-					input_require();
-				}
-				else{
-					sendMail(nom_prenom.value,mail.value,message.value);
-					succes(nom_prenom);
-					document.querySelector(".contact_form").reset();
-				}
-			});
-
+mail.addEventListener("input",e=>{
+	if(mail.value.match(regex_email)){
+			icon_valid.style.display="flex";
+			icon_invalid.style.display="none";
+			rep=true;
 		}
-		Valide();
+	else{
+			icon_invalid.style.display="flex";
+			icon_valid.style.display="none";
+			rep=false;
+		}
+	if(mail.value==""){
+		icon_valid.style.display="none";
+		icon_invalid.style.display="none";
+	}
+	});
+bouton.addEventListener("click",(e)=>{
+	e.preventDefault()
+
+	if(nom_prenom.value == "" || mail.value=="" || message.value=="" || rep==false)
+	{
+		input_require();
+	}
+	else{
+		sendMail(nom_prenom.value,mail.value,message.value);
+		succes(nom_prenom);
+		document.querySelector(".contact_form").reset();
+		icon_valid.style.display="none";
+	}
+});
+}
+	Valide();
 
 // End contact form
